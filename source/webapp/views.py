48ae73fae6ac934a223410ee86c18e8b90cbb539
Aziret Arlen <azerarlen312@gmail.com>
2020-08-19 15:03:51 +0600

    Создан Django-проект

---
+++ source/webapp/views.py
@@ -0,0 +1,110 @@
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from django.urls import reverse
+from django.utils.timezone import make_naive
+from django.views.generic import ListView, TemplateView, FormView
+
+from webapp.models import Article
+from .forms import ArticleForm, BROWSER_DATETIME_FORMAT, SimpleSearchForm
+from .base_views import FormView as CustomFormView, ListView as CustomListView
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'articles'
+    paginate_by = 2
+    paginate_orphans = 0
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        form = SimpleSearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            kwargs['search'] = search
+        kwargs['form'] = form
+        return super().get_context_data(object_list=object_list, **kwargs)
+
+    def get_queryset(self):
+        data = Article.objects.all()
+
+        if not self.request.GET.get('is_admin', None):
+            data = Article.objects.filter(status='moderated')
+
+        # http://localhost:8000/?search=ygjkjhg
+        form = SimpleSearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            if search:
+                data = data.filter(Q(title__icontains=search) | Q(author__icontains=search))
+
+        return data.order_by('-created_at')
+
+
+class ArticleView(TemplateView):
+    template_name = 'article_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pk = self.kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+
+        context['article'] = article
+        return context
+
+
+class ArticleCreateView(CustomFormView):
+    template_name = 'article_create.html'
+    form_class = ArticleForm
+
+    def form_valid(self, form):
+        self.article = form.save()
+        return super().form_valid(form)
+
+    def get_redirect_url(self):
+        return reverse('article_view', kwargs={'pk': self.article.pk})
+
+
+class ArticleUpdateView(FormView):
+    template_name = 'article_update.html'
+    form_class = ArticleForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.article = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['article'] = self.article
+        return context
+
+    def get_initial(self):
+        return {'publish_at': make_naive(self.article.publish_at)\
+            .strftime(BROWSER_DATETIME_FORMAT)}
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.article
+        return kwargs
+
+    def form_valid(self, form):
+        self.article = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('article_view', kwargs={'pk': self.article.pk})
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Article, pk=pk)
+
+
+def article_delete_view(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'article_delete.html', context={'article': article})
+    elif request.method == 'POST':
+        article.delete()
+        return redirect('index')
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
